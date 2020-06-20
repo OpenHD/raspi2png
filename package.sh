1167f0d6d9dff7671997d9bc3f9fc879bea782dc
Stephen Oliver <steve@infincia.com>
2020-06-20 12:17:20 -0400

    Add libpng package dependency

--- package.sh
+++ package.sh
@@ -27,7 +27,9 @@ VERSION=$(git describe)
 
 rm ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb > /dev/null 2>&1
 
-fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION//v} -C ${TMPDIR} -p ${PACKAGE_NAME}_VERSION_ARCH.deb || exit 1
+fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION//v} -C ${TMPDIR} \
+  -p ${PACKAGE_NAME}_VERSION_ARCH.deb \
+  -d "libpng-dev >= 1.16" || exit 1
 
 #
 # Only push to cloudsmith for tags. If you don't want something to be pushed to the repo, 
